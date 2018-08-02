e86f3a50d6e0acd7acab0f65d9649125bc91eb8f
Bumbleboss <Bumbleboss46@gmail.com>
2018-08-02 12:24:50 +0300

    Added + Patched up some stuff - Added scroll down button in projects page - Added links to tabs (Except Abouts as its not made yet) - Removed some stuff that are not going to be used - Adjusted the titles in sec_d for phone view - Changed tabs color for when its hovered on

--- frame.js
+++ frame.js
@@ -1,3 +1,10 @@
 $('#box_a3').click(function() {
   $(this).closest('.wrapper').toggleClass('burgered');
 });
+
+$(function() {
+  $('.scroll_down').click (function() {
+    $('html, body').animate({scrollTop: $('#sec_d').offset().top }, 'slow');
+    return false;
+  });
+});
